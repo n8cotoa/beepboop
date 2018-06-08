d461a83432a0d290894f20065fb07f33c558b30c
Nate Cottle <cottle.nate@gmail.com>
2018-06-08 13:28:34 -0700

    Add validation for name input, set foundation for adding name to output.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,9 @@ function replaceNum(input) {
   var numbersArray = makeNumberArray(input)
   var outputArray = []
   for (number = 0; number < numbersArray.length; ++number) {
+    var userName = name
     if ((number % 3) === 0 && number !== 0){
-      outputArray.push("Sorry Dave")
+      outputArray.push("I'm sorry, " + userName + ". I'm afraid I can't do that.")
     } else if (replaceOneOrZero(number, 1)) {
       outputArray.push("Boop!")
     } else if (replaceOneOrZero(number, 0)) {
@@ -33,22 +34,28 @@ function replaceOneOrZero(number, value) {
 
 function result(input) {
   var outputArray = replaceNum(input)
-  return outputArray.join(", ")
+  return outputArray.join(" ")
 
 
 }
 
 function flipIt(input) {
   var outputArray = replaceNum(input)
-  var flipArray = outputArray.reverse().join(", ")
+  var flipArray = outputArray.reverse().join(" ")
   return flipArray
 
 }
 
-function inputValidation(input) {
-  if (input < 0) {
+function isNumber(name) {
+  return !isNaN(parseFloat(name)) && isFinite(name);
+}
+
+function inputValidation(input, name) {
+  if (input < 0 || name === "") {
     return false
-  } else if (isNaN(input)) {
+  } else if (isNaN(input) || name === "") {
+    return false
+  } else if (isNumber(name)) {
     return false
   } else {
     return true
@@ -56,26 +63,27 @@ function inputValidation(input) {
 }
 
 $(document).ready(function() {
+
   $("#submitBtn").click(function(e) {
     e.preventDefault();
     var inputNum = parseInt($("#userInput").val());
-
-    if (inputValidation(inputNum)) {
+    var userName = $("#userName").val();
+    if (inputValidation(inputNum, userName)) {
       makeNumberArray(inputNum);
-      $("#results").text(result(inputNum))
+      $("#results").text(result(inputNum, userName))
     } else {
-      alert("Please enter a number higher than 0")
+      alert("Please enter your name and a number higher than 0")
     }
 
   });
   $("#flipBtn").click(function(e) {
     e.preventDefault();
     var inputNum = parseInt($("#userInput").val());
-    if (inputValidation(inputNum)) {
+    if (inputValidation(inputNum, userName)) {
       makeNumberArray(inputNum);
-      $("#results").text(flipIt(inputNum))
+      $("#results").text(flipIt(inputNum, userName))
     } else {
-      alert("Please enter a number higher than 0")
+      alert("Please enter your name and a number higher than 0")
     }
   });
 
